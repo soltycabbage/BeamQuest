9d787c16700ee0d5fa5bb7855ecf2c1f729c55ad
Toru Matsuoka <lettas0726@gmail.com>
2013-12-03 22:29:27 +0900

    Organize sample app

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , io = require('socket.io')
+var path     = require('path')
+  , express  = require('express')
+  , http     = require('http')
+  , socketIo = require('socket.io');
  
-var app = express()
-  , server = require('http').createServer(app)
-  , io = io.listen(server);
- 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
+var app = express();
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
   app.use(express.json());
   app.use(express.urlencoded());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
  
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure('development', function() {
+  app.use(express.errorHandler());
 });
  
-server.listen(app.get('port'))
+var server = http.createServer(app);
+server.listen(app.get('port'));
  
+var io = socketIo.listen(server);
 io.sockets.on('connection', function(socket) {
-  socket.on('message:send', function(data) {
-    io.sockets.emit('message:receive', { message: data.message });
-  });
+  socket.on('message:send', function(data) {
+    io.sockets.emit('message:receive', { message: data.message });
+  });
 });
+
