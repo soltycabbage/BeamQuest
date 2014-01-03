f29f9815a04931b47e7913a13f8efc93d2475c47
Toru Matsuoka <lettas0726@gmail.com>
2014-01-04 00:43:31 +0900

    Fix Idle animation

--- public/js/src/entity/player.js
+++ public/js/src/entity/player.js
@@ -123,12 +123,13 @@ bq.entity.Player = bq.entity.Entity.extend({
     getKeyFrameMap_: function () {
         return  {
             idle_bottom:      ["b0_0.png", "b0_1.png", "b0_2.png", "b0_3.png"],
+            idle_bottomright: ["b1_0.png", "b1_1.png", "b1_2.png", "b1_3.png"],
             idle_right:       ["b2_0.png", "b2_1.png", "b2_2.png", "b2_3.png"],
             idle_topright:    ["b3_0.png", "b3_1.png", "b3_2.png", "b3_3.png"],
             idle_top:         ["b4_0.png", "b4_1.png", "b4_2.png", "b4_3.png"],
             idle_topleft:     ["b5_0.png", "b5_1.png", "b5_2.png", "b5_3.png"],
             idle_left:        ["b6_0.png", "b6_1.png", "b6_2.png", "b6_3.png"],
-            idle_bottomleft:  ["b7_0.png", "b0_1.png", "b0_2.png", "b0_3.png"],
+            idle_bottomleft:  ["b7_0.png", "b7_1.png", "b7_2.png", "b7_3.png"],
             step_bottom:      ["b0_4.png", "b0_5.png", "b0_6.png", "b0_7.png"],
             step_bottomright: ["b1_4.png", "b1_5.png", "b1_6.png", "b1_7.png"],
             step_right:       ["b2_4.png", "b2_5.png", "b2_6.png", "b2_7.png"],
