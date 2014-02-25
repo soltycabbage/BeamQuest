6c131800e97ba439bc63114cf22735978c116e35
karihei <kariheeeei@gmail.com>
2014-02-25 12:29:00 +0900

    JSDocちゃんと書いた

--- public/js/src/entity/targetLine.js
+++ public/js/src/entity/targetLine.js
@@ -5,6 +5,7 @@ bq.entity.TargetLine = cc.Node.extend({
     /**
      * @param {bq.entity.Entity} source
      * @param {bq.entity.Entity} target
+     * @constructor
      */
     ctor: function(source, target) {
         this._super();
@@ -25,25 +26,51 @@ bq.entity.TargetLine = cc.Node.extend({
 
         /**
          * 線分の末尾
+         * @type {cc.Sprite}
          * @private
          */
         this.tail_;
 
         /**
          * 線分先頭
+         * @type {cc.Sprite}
          * @private
          */
         this.head_;
 
+        /**
+         * 線分
+         * @type {cc.DrawNode}
+         * @private
+         */
+        this.line_;
+
+        /**
+         * @type {number}
+         * @private
+         */
         this.step_ = 1;
+
+        /**
+         * sourceからtargetに達するまでのステップ数
+         * @type {number}
+         * @private
+         */
         this.maxStep_ = 10;
 
-        this.scheduleUpdate();
-        this.schedule(this.clearLine, 3);
+        /**
+         * タゲ開始したらtrue
+         * @type {boolean}
+         * @private
+         */
         this.isActive_ = false;
+
+        this.scheduleUpdate();
+        this.schedule(this.clearLine, 3.0, 0, 0);
         bq.baseLayer.addChild(this);
     },
 
+    /** @override */
     update: function() {
         if (this.isActive_) {
             this.updateLine_();
@@ -53,6 +80,36 @@ bq.entity.TargetLine = cc.Node.extend({
         }
     },
 
+    /**
+     * タゲ開始
+     */
+    drawLine: function() {
+        this.initLine_();
+        bq.soundManager.playEffect(s_SeTargetLine);
+        this.isActive_ = true;
+    },
+
+    /**
+     * ターゲットラインを消す
+     */
+    clearLine: function() {
+        var fadeOut = cc.FadeOut.create(0.5);
+        var callFunc = cc.CallFunc.create(function() {
+            this.isActive_ = false;
+            this.line_.clear();
+            this.step_ = 1;
+            this.tail_.removeFromParent();
+            this.head_.removeFromParent();
+            this.line_.removeFromParent();
+        }.bind(this));
+
+        this.runAction(cc.Sequence.create(fadeOut, callFunc));
+    },
+
+    /**
+     * tail、head、lineの位置をsourceとtargetの位置と同期させる
+     * @private
+     */
     initLine_: function() {
         var head = cc.Sprite.create();
         head.setTextureRect(cc.rect(0,0,3,3));
@@ -70,6 +127,10 @@ bq.entity.TargetLine = cc.Node.extend({
         this.addChild(this.line_, 10);
     },
 
+    /**
+     * 現在のstep数に応じてラインをニョーンって伸ばしていく
+     * @private
+     */
     updateLine_: function() {
         var p1 = this.source_.getPosition();
         var p2 = this.target_.getPosition();
@@ -82,28 +143,5 @@ bq.entity.TargetLine = cc.Node.extend({
         this.line_.clear();
         this.line_.drawSegment(this.tail_.getPosition(), this.head_.getPosition(), 2, cc.c4f(1, 0, 0, 0.6));
         this.line_.drawSegment(this.tail_.getPosition(), this.head_.getPosition(), 1, cc.c4f(1, 0.8, 0.8, 0.7));
-    },
-
-    /**
-     */
-    drawLine: function() {
-        this.initLine_();
-        bq.soundManager.playEffect(s_SeTargetLine);
-        this.isActive_ = true;
-    },
-
-    clearLine: function() {
-        var fadeOut = cc.FadeOut.create(0.5);
-        var callFunc = cc.CallFunc.create(function() {
-            this.isActive_ = false;
-            this.line_.clear();
-            this.step_ = 1;
-            this.tail_.removeFromParent();
-            this.head_.removeFromParent();
-            this.line_.removeFromParent();
-        }.bind(this));
-
-        this.runAction(cc.Sequence.create(fadeOut, callFunc));
     }
-
 });
