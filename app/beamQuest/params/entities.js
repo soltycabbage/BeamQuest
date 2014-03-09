94462c2557d6a5a41f30ee04931e60f301fc5918
karihei <kariheeeei@gmail.com>
2014-03-09 14:19:47 +0900

    超絶レベルアップキャンペーン　糸冬　了

--- app/beamQuest/params/entities.js
+++ app/beamQuest/params/entities.js
@@ -1,4 +1,4 @@
 bq.params.Entities = {
     // Mobs
-    KAMUTARO: {id: 'mob_kamutaro', name: 'カム太郎', hp: 100, attack: 20, defence:10, exp: 2000}
+    KAMUTARO: {id: 'mob_kamutaro', name: 'カム太郎', hp: 100, attack: 20, defence:10, exp: 10}
 };
