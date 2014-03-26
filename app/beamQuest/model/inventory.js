fe296c3aae67fd6d442f8e3ca5e51fb830a46969
karihei <kariheeeei@gmail.com>
2014-03-26 22:12:16 +0900

    インベントリの管理

---
+++ app/beamQuest/model/inventory.js
@@ -0,0 +1,63 @@
+var util = require('util'),
+    Model = require('beamQuest/model/model');
+
+/**
+ * インベントリのmodel
+ * @constructor
+ * @extends {model.Model}
+ */
+var Inventory = function(opt_data) {
+    Model.apply(this, arguments);
+
+    /**
+     * 所持品
+     * @type {Object}
+     */
+    this.items = this.data.items || {};
+
+    /**
+     * 所持金
+     * @type {number}
+     */
+    this.money = this.data.money || 0;
+};
+util.inherits(Inventory(), Model);
+
+/**
+ * アイテムを加える
+ * @param {string} itemId
+ */
+Inventory.prototype.pushItem = function(itemId) {
+    if (!this.items[itemId]) {
+        this.items[itemId] = {'amount': 1};
+    } else {
+        this.items[itemId].amount++;
+    }
+};
+
+/**
+ * アイテムを取り出す
+ * @param {string} itemId
+ * @return {?string} アイテムID
+ */
+Inventory.prototype.pickItem = function(itemId) {
+    if (!this.items[itemId]) {
+        return null;
+    } else {
+        this.items[itemId].amount--;
+        if (this.items[itemId].amount <= 0) {
+            delete this.items[itemid];
+        }
+        return itemId;
+    }
+};
+
+/** @override */
+Inventory.prototype.toJSON = function() {
+    var json = {};
+    json.items = this.items;
+    json.mony = this.money;
+    return json;
+};
+
+module.exports = Inventory;
---
+++ app/beamQuest/store/inventory.js
@@ -0,0 +1,53 @@
+var InventoryModel = require('beamQuest/model/inventory');
+
+/**
+ * 各プレイヤーの所持品を保持しておくクラス
+ * @constructor
+ */
+var Inventory = function() {
+
+    /**
+     * こんな感じでアイテムを保持する
+     {
+         'karihei': {
+             'items': {
+                 '1':  {'amount': 10}, // アイテムIDと数量
+                 '12': {'amount': 5},  // IDをキーにしてるのは検索しやすくするため。
+                 '13': {'amount': 3}
+             },
+             'money': 10000 // ビーツ
+         }
+     };
+     */
+    this.inventory_;
+};
+
+/**
+ * アイテムを所持品に加える
+ * @param {string} userId
+ * @param {string} itemId
+ */
+Inventory.prototype.push = function(userId, itemId) {
+    if (!this.inventory_[userId]) {
+        this.inventory_[userId] = new InventoryModel();
+    }
+
+    this.inventory_[userId].pushItem(itemId);
+};
+
+/**
+ * アイテムを所持品から取り出す
+ * @param {string} userId
+ * @param {string} itemId
+ * @return {?string} アイテムID
+ */
+Inventory.prototype.pick = function(userId, itemId) {
+    if (!this.inventory_[userId]) {
+        return null;
+    }
+
+    return this.inventory_[userId].pickItem(itemId);
+};
+var instance_ = new Inventory();
+
+module.exports = instance_;
