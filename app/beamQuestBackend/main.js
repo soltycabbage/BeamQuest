a1700cd3e86e306a030e47254c27ab3b2b7fa764
nise_nabe <nise.nabe@gmail.com>
2014-04-17 13:26:17 +0900

    upgrade express
    問題: リクエストが処理されなくなった

--- app/beamQuestBackend/main.js
+++ app/beamQuestBackend/main.js
@@ -1,18 +1,21 @@
-var kvs = require('beamQuest/store/kvs').createClient();
+var router = require('express').Router(),
+    kvs = require('beamQuest/store/kvs').createClient();
 
-exports.listen = function(app) {
+module.exports = function(app) {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
 
-    app.get('/backend', function(req, res) {
+    router.get('/', function(req, res) {
         res.render('index', {env: process.env.NODE_ENV});
     });
 
-    app.post('/backend/kvs/purge', function(req, res) {
+    router.post('/kvs/purge', function(req, res) {
         kvs.flushall(function(didSucceed) {
             "use strict";
 
             res.json({});
         });
     });
+
+    return router;
 };
