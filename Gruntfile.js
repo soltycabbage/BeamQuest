c4383cc1fd310fcf6deb155f4f5059d3c40bcbf6
nise_nabe <nise.nabe@gmail.com>
2014-04-30 17:15:39 +0900

    Gruntfile.js のインデントを 4 に合わせる

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,54 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-mocha-test');
 
     grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
 
-    watch: {
-      sass: {
-        files: 'public/scss/*.scss',
-        tasks: ['compass']
-      },
+        watch: {
+            sass: {
+                files: 'public/scss/*.scss',
+                tasks: ['compass']
+            },
 
-      scripts: {
-        files: ['app/**/*.js', 'public/**/*.js'],
-        tasks: ['jshint']
-      },
+            scripts: {
+                files: ['app/**/*.js', 'public/**/*.js'],
+                tasks: ['jshint']
+            },
 
-      options: {
-        nospawn: true
-      }
-    },
-
-    jshint: {
-      all: ['<%= watch.scripts.files %>'],
-      options: grunt.file.readJSON('.jshintrc')
-    },
-
-    compass: {
-      dist: {
-        options: {
-          config: 'config.rb'
-        }
-      }
-    },
-
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
+            options: {
+                nospawn: true
+            }
         },
-        src: ['app/**/*Spec.js']
-      }
-    }
-  });
 
-  grunt.event.on('watch', function(action, filepath) {
-      grunt.config(['jshint', 'all'], [filepath]);
-  });
+        jshint: {
+            all: ['<%= watch.scripts.files %>'],
+            options: grunt.file.readJSON('.jshintrc')
+        },
 
-  grunt.registerTask('test', ['jshint', 'mochaTest']);
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb'
+                }
+            }
+        },
+
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['app/**/*Spec.js']
+            }
+        }
+    });
+
+    grunt.event.on('watch', function(action, filepath) {
+        grunt.config(['jshint', 'all'], [filepath]);
+    });
+
+    grunt.registerTask('test', ['jshint', 'mochaTest']);
 };
