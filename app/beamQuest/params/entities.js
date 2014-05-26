ce5a1b31790d759619d5ffc8fa2b063a5762509d
karihei <kariheeeei@gmail.com>
2014-05-26 20:51:17 +0900

    カム太郎がクソ強かったのを元に戻した

--- app/beamQuest/params/entities.js
+++ app/beamQuest/params/entities.js
@@ -7,7 +7,7 @@ bq.params.Entities = {
         // id: 識別用ID、 name: 表示名,
         id: 'mob_kamutaro', name: 'カム太郎',
         // hp: 体力, attack: 攻撃力, defence: 防御力, exp: もらえる経験値, money: 落とすビーツの量
-        hp: 500, attack: 20, defence:10, exp: 3, money: 30,
+        hp: 100, attack: 20, defence:10, exp: 3, money: 30,
         drop: [ // drop: 落とすアイテムのリスト
             // id: アイテムID, rate: 落とす割合（この場合は10分の1)
             {id: bq.Types.Items.POTION, rate: 1}
