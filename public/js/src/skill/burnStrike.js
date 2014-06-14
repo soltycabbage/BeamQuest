741916aad3c4cacae0ecb84f23b644dfce684106
karihei <kariheeeei@gmail.com>
2014-06-14 22:39:14 +0900

    skill基底クラスににfireメソッドを実装

--- public/js/src/skill/burnStrike.js
+++ public/js/src/skill/burnStrike.js
@@ -1,6 +1,6 @@
 bq.skill.BurnStrike = bq.skill.extend({
     /** @override */
     fire: function() {
-        alert(1);
+        
     }
 });
--- public/js/src/skill/skill.js
+++ public/js/src/skill/skill.js
@@ -1,5 +1,21 @@
 bq.skill = cc.Class.extend({
+    /**
+     * @param {!bq.model.Skill} model
+     * @param {!bq.model.Entity} user
+     * @param {!bq.model.Position} targetPos
+     */
+    ctor: function(model, user, targetPos) {
+        /** @protected */
+        this.model = model;
 
+        /** @protected */
+        this.user = user;
+
+        /** @protected */
+        this.targetPos = targetPos;
+    },
     /** @protected */
-    fire: function() {}
+    fire: function() {
+
+    }
 });
