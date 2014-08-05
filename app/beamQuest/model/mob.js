e959b9446f5acbe1e6ec37472e5de785d5abd882
nise_nabe <nise.nabe@gmail.com>
2014-08-06 01:40:00 +0900

    mob.js を TypeScript 化

--- app/beamQuest/model/mob.js
+++ app/beamQuest/model/mob.js
@@ -1,61 +1,32 @@
-var util = require('util'),
-    Entity = require('beamQuest/model/entity');
-
-/**
- * @constructor
- * @extends {model.Entity}
- */
-var Mob = function(opt_data) {
-    Entity.apply(this, arguments);
-
-    /** @override */
-    this.type = bq.Types.EntityType.MOB;
-
-    /**
-     * 獲得経験値的な
-     * @type {number}
-     */
-    this.exp = this.data.exp || Mob.DEFAULT_EXP;
-
-    /**
-     * 移動速度
-     * @type {number}
-     */
-    this.moveSpeed = 5;
-
-    /**
-     * trueなら攻撃を受けるまで敵対行動を取らないタイプのmob
-     * @type {Boolean}
-     */
-    this.isPassive = true;
-
-    /**
-     * ドロップするアイテムのリスト
-     * @type {Array.<Object>}
-     */
-    this.drop = this.data.drop || [];
-
-    /**
-     * ドロップするビーツの量
-     * @type {number}
-     */
-    this.money = this.data.money || 0;
-
-    /**
-     * 敵の場合は最大HPを設定値から取得してくる
-     * @type {number}
-     */
-    this.maxHp = _.isUndefined(this.data.hp) ? this.maxHp : this.data.hp;
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
 };
-util.inherits(Mob, Entity);
+var Entity = require('beamQuest/model/entity');
 
-Mob.DEFAULT_EXP = 1;
+var Mob = (function (_super) {
+    __extends(Mob, _super);
+    function Mob(opt_dat) {
+        _super.call(this, opt_dat);
 
-/** @override */
-Mob.prototype.toJSON = function() {
-    var json = Mob.super_.prototype.toJSON.apply(this);
-    json.exp = this.exp;
-    return json;
-};
+        this.type = bq.Types.EntityType.MOB;
+        this.exp = this.data.exp || Mob.DEFAULT_EXP;
+        this.moveSpeed = 5;
+        this.isPassive = true;
+        this.drop = this.data.drop || [];
+        this.money = this.data.money || 0;
+        this.maxHp = _.isUndefined(this.data.hp) ? this.maxHp : this.data.hp;
+    }
+    Mob.prototype.toJSON = function () {
+        var json = _super.prototype.toJSON.call(this);
+        json.exp = this.exp;
+        return json;
+    };
+    Mob.DEFAULT_EXP = 1;
+    return Mob;
+})(Entity);
 
 module.exports = Mob;
+//# sourceMappingURL=mob.js.map
