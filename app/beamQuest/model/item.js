c4f1cfc2711c4d7ca1d694787d00f057f2fdcf44
nise_nabe <nise.nabe@gmail.com>
2014-08-05 23:13:44 +0900

    item.js を TypeScript 化

--- app/beamQuest/model/item.js
+++ app/beamQuest/model/item.js
@@ -1,41 +1,32 @@
-var util = require('util'),
-    Model = require('beamQuest/model/model');
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Model = require('beamQuest/model/model');
 
 /**
- * アイテムのmodel
- * @constructor
- * @extends {model.Model}
- */
-var Item = function(opt_data) {
-    Model.apply(this, arguments);
+* アイテムのmodel
+*/
+var Item = (function (_super) {
+    __extends(Item, _super);
+    function Item(opt_data) {
+        _super.call(this, opt_data);
 
-    /**
-     * アイテムの名前
-     * @type {string}
-     */
-    this.name = this.data.name || 'item';
-
-    /**
-     * アイテムの説明文
-     * @type {string}
-     */
-    this.info = this.data.info || '';
-
-    /**
-     * アイテムのタイプ
-     * @type {bq.Types.ItemType}
-     */
-    this.type = this.data.type;
-};
-util.inherits(Item, Model);
-
-/** @override */
-Item.prototype.toJSON = function() {
-    var json = {};
-    json.name = this.name;
-    json.info = this.info;
-    json.type = this.type;
-    return json;
-};
+        this.name = this.data.name || 'item';
+        this.info = this.data.info || '';
+        this.type = this.data.type;
+    }
+    Item.prototype.toJSON = function () {
+        var json = _super.prototype.toJSON.call(this);
+        json.name = this.name;
+        json.info = this.info;
+        json.type = this.type;
+        return json;
+    };
+    return Item;
+})(Model);
 
 module.exports = Item;
+//# sourceMappingURL=item.js.map
