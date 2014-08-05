5992916c55f33ae971b45fb41d0c93a4e72926b8
nise_nabe <nise.nabe@gmail.com>
2014-08-05 17:46:44 +0900

    invendory.js を TypeScript 化

---
+++ app/beamQuest/model/inventory.ts
@@ -0,0 +1,57 @@
+import Model = require('beamQuest/model/model');
+
+/**
+ * インベントリのmodel
+ */
+class Inventory extends Model {
+    /** 所持品 */
+    items: Object;
+
+    /** 所持金 */
+    money: Number;
+
+    constructor(opt_data) {
+        super(opt_data);
+
+        this.items = this.data.items || {};
+        this.money = this.data.money || 0;
+    }
+
+    /**
+     * アイテムを取り出す
+     * @param itemId
+     */
+    pushItem(itemId: string): void {
+        if (!this.items[itemId]) {
+            this.items[itemId] = {'amount': 1};
+        } else {
+            this.items[itemId].amount++;
+        }
+    }
+
+    /**
+     * アイテムを取り出す
+     * @param itemId
+     * @returns {?string}
+     */
+    pickItem(itemId: string): any {
+        if (!this.items[itemId]) {
+            return null;
+        } else {
+            this.items[itemId].amount--;
+            if (this.items[itemId].amount <= 0) {
+                delete this.items[itemId];
+            }
+            return itemId;
+        }
+    }
+
+    toJSON() : any{
+        var json = super.toJSON();
+        json.items = this.items;
+        json.mony = this.money;
+        return json;
+    }
+}
+
+export = Inventory;
