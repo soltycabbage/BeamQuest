377467eff4d4bbe2251e499fdaee0b9a51bbca86
nise_nabe <nise.nabe@gmail.com>
2014-08-09 15:35:00 +0900

    Scheduler あたりを TypeScript 化

---
+++ app/beamQuest/scheduleTarget.ts
@@ -0,0 +1,37 @@
+import Scheduler = require('beamQuest/scheduler')
+
+/**
+ * メインループで実行されるクラスはこいつを継承する
+ */
+class ScheduleTarget {
+    private scheduler;
+
+    constructor() {
+        this.scheduler = Scheduler.getInstance();
+    }
+
+    /**
+     * ループ毎にスケジューラにupdate()を実行させたい場合はこれを呼ぶ
+     */
+    scheduleUpdate() {
+        this.scheduler.add(this);
+    }
+
+    /**
+     * 登録したupdate()スケジュールを削除したい場合はこれを呼ぶ
+     */
+    unscheduleUpdate() {
+        this.scheduler.remove(this);
+    }
+
+    /**
+     * 毎ループ実行される
+     * @protected
+     */
+    update() {
+        // 継承先でoverrideしてね
+    }
+
+}
+
+export = ScheduleTarget;
