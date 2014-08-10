d204b7244ede04b2a45f911ff4eb970660302a24
nise_nabe <nise.nabe@gmail.com>
2014-08-10 23:02:58 +0900

    node_redis を tsd で追加してる情報のコミット忘れ

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="body-parser/body-parser.d.ts" />
 /// <reference path="socket.io/socket.io.d.ts" />
 /// <reference path="underscore/underscore.d.ts" />
+/// <reference path="node_redis/node_redis.d.ts" />
