f6118f285e296728eb8b65c96c5da9c14a1709e2
nise_nabe <nise.nabe@gmail.com>
2014-08-12 22:30:02 +0900

    ctrl/entity.js を TypeScript 化

--- app/beamQuest/ctrl/entity.js
+++ app/beamQuest/ctrl/entity.js
@@ -1,51 +1,58 @@
-var util = require('util'),
-    EntityModel = require('beamQuest/model/entity'),
-    ScheduleTarget = require('beamQuest/scheduleTarget'),
-    EntityListener = require('beamQuest/listener/entity');
-
-/**
- * すべてのmob、playerの基底クラス。
- * HPの増減、ステータス異常、死亡処理など敵味方関係のない共通の処理はここに書く
- * @constructor
- * @param {Object} modelData
- * @extends {bq.ScheduleTarget}
- */
-var Entity = function(modelData) {
-    ScheduleTarget.apply(this, arguments);
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
 };
-util.inherits(Entity, ScheduleTarget);
+var ScheduleTarget = require('beamQuest/scheduleTarget');
+var EntityListener = require('beamQuest/listener/entity');
 
 /**
- * ctrlをnewしたあとは必ずこれを呼んでmodel(各種ステータスとか)をセットすること
- * @param {model.Entity} model
- */
-Entity.prototype.setModel = function(model) {
-    this.model = model;
-
-    this.model.on('addHp', _.bind(this.handleAddHp, this));
-};
-
-/**
- * @param {number} amount
- * @param {boolean} isCritical
- * @protected
- */
-Entity.prototype.handleAddHp = function(amount, isCritical) {
-    var hpData = [
-        {entity: this.model, hpAmount: amount, isCritical: isCritical}
-    ];
-    EntityListener.getInstance().updateHp(hpData);
-    if (this.model.hp <= 0) {
-        this.death();
+* すべてのmob、playerの基底クラス。
+* HPの増減、ステータス異常、死亡処理など敵味方関係のない共通の処理はここに書く
+* @constructor
+* @param {Object} modelData
+* @extends {bq.ScheduleTarget}
+*/
+var Entity = (function (_super) {
+    __extends(Entity, _super);
+    function Entity() {
+        _super.apply(this, arguments);
     }
-};
+    /**
+    * ctrlをnewしたあとは必ずこれを呼んでmodel(各種ステータスとか)をセットすること
+    * @param {model.Entity} model
+    */
+    Entity.prototype.setModel = function (model) {
+        this.model = model;
 
-/**
- * 死亡フラグ
- * @protected
- */
-Entity.prototype.death = function() {
-    // playerとかmobとかでoverrideする
-};
+        this.model.on('addHp', _.bind(this.handleAddHp, this));
+    };
+
+    /**
+    * @param {number} amount
+    * @param {boolean} isCritical
+    * @protected
+    */
+    Entity.prototype.handleAddHp = function (amount, isCritical) {
+        var hpData = [
+            { entity: this.model, hpAmount: amount, isCritical: isCritical }
+        ];
+        EntityListener.getInstance().updateHp(hpData);
+        if (this.model.hp <= 0) {
+            this.death();
+        }
+    };
+
+    /**
+    * 死亡フラグ
+    * @protected
+    */
+    Entity.prototype.death = function () {
+        // playerとかmobとかでoverrideする
+    };
+    return Entity;
+})(ScheduleTarget);
 
 module.exports = Entity;
+//# sourceMappingURL=entity.js.map
---
+++ app/beamQuest/ctrl/entity.ts
@@ -0,0 +1,49 @@
+import EntityModel = require('beamQuest/model/entity');
+import ScheduleTarget = require('beamQuest/scheduleTarget');
+import EntityListener = require('beamQuest/listener/entity');
+
+/**
+ * すべてのmob、playerの基底クラス。
+ * HPの増減、ステータス異常、死亡処理など敵味方関係のない共通の処理はここに書く
+ * @constructor
+ * @param {Object} modelData
+ * @extends {bq.ScheduleTarget}
+ */
+class Entity extends ScheduleTarget {
+    model: any; // EntityModel だったもの
+
+    /**
+     * ctrlをnewしたあとは必ずこれを呼んでmodel(各種ステータスとか)をセットすること
+     * @param {model.Entity} model
+     */
+    setModel(model:EntityModel) {
+        this.model = model;
+
+        this.model.on('addHp', _.bind(this.handleAddHp, this));
+    }
+
+    /**
+     * @param {number} amount
+     * @param {boolean} isCritical
+     * @protected
+     */
+     handleAddHp(amount, isCritical) {
+        var hpData = [
+            {entity: this.model, hpAmount: amount, isCritical: isCritical}
+        ];
+        EntityListener.getInstance().updateHp(hpData);
+        if (this.model.hp <= 0) {
+            this.death();
+        }
+    }
+
+    /**
+     * 死亡フラグ
+     * @protected
+     */
+    death() {
+        // playerとかmobとかでoverrideする
+    }
+}
+
+export = Entity;
