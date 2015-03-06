c75cd41cb0a2b88c21affb640954dde10b470590
nise_nabe <nise.nabe@gmail.com>
2015-03-07 00:57:43 +0900

    デフォルトの port 間違ってた

--- config/test.js
+++ config/test.js
@@ -2,11 +2,11 @@ module.exports = {
     kvs: {
         type: "redis",
         host: "localhost",
-        port: 6380
+        port: 6379
     },
     session: {
         type: "redis",
         host: "localhost",
-        port: 6380
+        port: 6379
     }
 };
